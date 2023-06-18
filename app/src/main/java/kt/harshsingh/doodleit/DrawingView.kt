aa19c4a98f2db6d133254cd5bb4115dc32f61509
Harsh Singh <89487399+harshsingh-io@users.noreply.github.com>
2023-06-18 15:07:08 +0530

    Implementing Color Picker Feature

--- app/src/main/java/kt/harshsingh/doodleit/DrawingView.kt
+++ app/src/main/java/kt/harshsingh/doodleit/DrawingView.kt
@@ -98,7 +98,11 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
         mBrushSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,newSize,resources.displayMetrics)
         mDrawPaint!!.strokeWidth = mBrushSize
     }
+    fun setColor(newColor: String){
+        color = Color.parseColor(newColor)
+        mDrawPaint!!.color = color
 
+    }
     internal inner class CustomPath(var color: Int, var brushThickness: Float) : Path() {
 
     }
